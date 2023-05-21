d93b336ce69efa642d9283c965f50b8659b4417c
rmb <rmb.null24@gmail.com>
2023-05-20 23:39:56 -0600

    manejoErrores

--- src/controllers/producto.ts
+++ src/controllers/producto.ts
@@ -2,13 +2,20 @@ import { Request, Response } from "express";
 import { pool } from "../../db";
 
 export const productos = async (_: Request, res: Response) => {
-  const result = await pool.query("SELECT * FROM TProducto ORDER BY codigo");
-  res.json(result[0]);
+  try {
+    const result = await pool.query("SELECT * FROM TProducto");
+    res.send(result[0]);
+  } catch (error) {
+    console.log(error);
+    res.json({
+      message: "Error al obtener productos",
+    });
+  }
 };
 
 export const producto = async (req: Request, res: Response) => {
+  const { codigo } = req.params;
   try {
-    const { codigo } = req.params;
     const result = await pool.query(
       "SELECT * FROM TProducto WHERE codigo = ?",
       [codigo]
@@ -16,17 +23,20 @@ export const producto = async (req: Request, res: Response) => {
     res.send(result[0]);
   } catch (error) {
     console.log(error);
+    res.json({
+      message: "Error al obtener producto",
+    });
   }
 };
 
 // Crear producto Codigo, Nombre, Precio
 export const crearProducto = async (req: Request, res: Response) => {
+  const producto = {
+    codigo: req.body.codigo,
+    nombre: req.body.nombre,
+    precio: req.body.precio,
+  };
   try {
-    const producto = {
-      codigo: req.body.codigo,
-      nombre: req.body.nombre,
-      precio: req.body.precio,
-    };
     console.log(producto);
     await pool.query(
       "INSERT INTO TProducto (codigo, nombre, precio) VALUES (?, ?, ?)",
@@ -37,33 +47,46 @@ export const crearProducto = async (req: Request, res: Response) => {
     });
   } catch (error) {
     console.log(error);
+    res.json({
+      message: "Error al crear producto",
+    });
   }
 };
 
 export const actualizarProducto = async (req: Request, res: Response) => {
-  const producto = {
-    codigo: req.body.codigo,
-    nombre: req.body.nombre,
-    precio: req.body.precio,
-  };
   const { codigo } = req.params;
-  // actualizar solo nombre y precio
-  await pool.query(
-    "UPDATE TProducto SET nombre = ?, precio = ? WHERE codigo = ?",
-    [producto.nombre, producto.precio, codigo]
-  );
+  try {
+    const producto = {
+      codigo: req.body.codigo,
+      nombre: req.body.nombre,
+      precio: req.body.precio,
+    };
+    // actualizar solo nombre y precio
+    await pool.query(
+      "UPDATE TProducto SET nombre = ?, precio = ? WHERE codigo = ?",
+      [producto.nombre, producto.precio, codigo]
+    );
 
-  res.json({ message: "Producto actualizado" });
+    res.json({ message: "Producto actualizado" });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      message: "Error al actualizar producto",
+    });
+  }
 };
 
 export const eliminarProducto = async (req: Request, res: Response) => {
+  const { codigo } = req.params;
   try {
-    const { codigo } = req.params;
     await pool.query("DELETE FROM TProducto WHERE codigo = ?", [codigo]);
     res.json({
       message: "Producto eliminado",
     });
   } catch (error) {
     console.log(error);
+    res.json({
+      message: "Error al eliminar producto",
+    });
   }
 };
